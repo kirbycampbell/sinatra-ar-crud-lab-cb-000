a2f5679bcf981af418d76c66750f188e5365a612
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-27 05:22:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,5 +46,11 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.delete
+    erb :delete
+  end
+
 
 end
